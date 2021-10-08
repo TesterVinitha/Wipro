28198eddfcf22adcce236f371351755fcd44626b
TesterVinitha <vinithalaxmi2021@gmail.com>
2021-10-08 11:57:10 +0530

    User id Functionality

--- src/wirproPakage/WiproClass.java
+++ src/wirproPakage/WiproClass.java
@@ -2,4 +2,15 @@ package wirproPakage;
 
 public class WiproClass {
 
+	public static void Userid() {
+
+		System.out.println("id");
+
+	}
+
+	public static void main(String[] args) {
+
+		Userid();
+	}
+
 }
